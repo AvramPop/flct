9c0c09a9120d70f6f81b9f162aa2611c697903c3
AvramPop <daniel.avram.pop@gmail.com>
2020-10-14 09:15:23 +0300

    added tests for lab3

--- lab3/src/com/company/Main.java
+++ lab3/src/com/company/Main.java
@@ -3,10 +3,24 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println(test());
+    }
+
+    private static boolean test() {
+        boolean result = true;
         SymbolTable symbolTable = new SymbolTable(10);
         symbolTable.add("5");
-        symbolTable.add("5");
-        System.out.println(symbolTable.toString());
-        System.out.println(symbolTable.indexOf("5"));
+        symbolTable.add("while");
+        symbolTable.add("i");
+        symbolTable.add("true");
+        result &= symbolTable.contains("5");
+        result &= symbolTable.contains("while");
+        result &= !symbolTable.contains("jk");
+        symbolTable.remove("5");
+        result &= !symbolTable.contains("5");
+        result &= symbolTable.indexOf("i").getKey() == 5;
+        result &= symbolTable.indexOf("i").getValue() == 0;
+        return result;
     }
+
 }
